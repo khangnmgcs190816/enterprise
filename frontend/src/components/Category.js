f2fe3a06cedf696f4f51610ab4448b08a4a43cfd
kieuthy <thynmkgcs190847@fpt.edu.vn>
2022-02-21 06:32:30 +0700

    create category

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -1,9 +1,44 @@
 import React from 'react'
-
+import { useState } from "react";
 const Category = () => {
+  var [category, setCategory] = useState([]);
+  const [id, setId]=useState();
+  const [tag, setTag]=useState();
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const category = {
+      id,
+      tag
+    };
+
+    setIsPending(true);
+
+    fetch("http://localhost:8080/category", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(idea),
+    }).then(() => {
+      console.log(idea);
+      console.log("new category added");
+      setIsPending(false);
+    });
+  };
   return (
-    <div>Category</div>
+    <form  onSubmit={handleSubmit}>
+      <div>Category
+        <textarea id="outlined-basic"
+              variant="outlined"
+              type="text"
+              name="user"
+              placeholder={category}
+              onChange={(e) => setCategory(e.target.value)}/>
+      </div>
+      { !isPending && <button >Submit</button>}
+      { isPending && <button disabled>Submitting...</button>}
+    </form>
   )
 }
 
-export default Category
+export default Category;
