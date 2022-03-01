4d03507b9ec4a2693ca17e1f3f3955e8a0b5c946
khang-nghiem <31397614+khang-nghiem@users.noreply.github.com>
2022-03-01 20:45:59 +0700

    babel setup

---
+++ frontend/tests/Login.test.js
@@ -0,0 +1,44 @@
+import LoginForm from "../src/pages/Login/LoginForm";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { cleanup, screen, fireEvent, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+
+
+// afterEach(() => {
+//     document.body.removeChild(container);
+//     container = null;
+// });
+
+const setup = () => render(<LoginForm />);
+
+
+
+describe('LoginForm contains required elements', () => {
+    it('Check if the page has an input for Username', () => {
+        setup();
+        expect(screen.getByText('Welcome to FPT Greenwich!')).toBeInTheDocument();
+        expect(screen.getByPlaceholderText('Username')).toBeInTheDocument();
+    });
+    it('Check if the page has an input for Password', () => {
+        setup();
+        expect(screen.getByText('Welcome to FPT Greenwich!')).toBeInTheDocument();
+        expect(screen.getByPlaceholderText('Password')).toBeInTheDocument();
+    });
+    it('Check if the page has a Login button', () => {
+        setup();
+        const loginTrigger = screen.getByText('Login', { selector: 'button' });
+        // expect(loginTrigger).toBeDefined();
+        expect(loginTrigger).not.toBe(null);
+    });
+
+    it('check if TextField exists', () => {
+        setup();
+        expect(screen.getByTestId('username')).toBeInTheDocument();
+    });
+
+
+
+})
+
