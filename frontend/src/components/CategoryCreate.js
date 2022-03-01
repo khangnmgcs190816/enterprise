5a25dc2d3f43e3ffcf72bd4667b3691f300507f2
pngoc.khanhvy <53236281+pnkv12@users.noreply.github.com>
2022-03-01 21:01:13 +0700

    Update Mui for various pages

--- frontend/src/components/CategoryCreate.js
+++ frontend/src/components/CategoryCreate.js
@@ -8,134 +8,135 @@ import { lightBlue } from "@mui/material/colors";
 import SendIcon from "@mui/icons-material/Send";
 import { Link as RouterLink } from "react-router-dom";
 import axios from "axios";
+// import Category from "./Category/Category";
 
 // LINK CATEGORY NẰM Ở DROPDOWN USER NHÁ!!!!!!
 
 const TitleFrame = styled("div")({
-    color: lightBlue[600],
-    // textAlign: "center",
-    fontSize: 30,
-    fontWeight: "bold",
-    marginBottom: "1rem",
+  color: lightBlue[600],
+  // textAlign: "center",
+  fontSize: 30,
+  fontWeight: "bold",
+  marginBottom: "1rem",
 });
 
 const CategoryCreate = () => {
-    var date = new Date(); //Cái nì là chứa ngày tháng cho cái created_at nè
+  var date = new Date(); //Cái nì là chứa ngày tháng cho cái created_at nè
 
-    const [categoryName, setCategoryName] = useState("Unknown category"); //cái nì là cái chứa tên category(?), đang bị lú chỗ này
-    const [created_at, setCreateDate] = useState(date);
-    const [updated_at, setUpdateDate] = useState();
-    const [isPending, setIsPending] = useState(false);
-    // Thêm const "used"(?)
+  const [categoryName, setCategoryName] = useState("Unknown category"); //cái nì là cái chứa tên category(?), đang bị lú chỗ này
+  const [created_at, setCreateDate] = useState(date);
+  const [updated_at, setUpdateDate] = useState();
+  const [isPending, setIsPending] = useState(false);
+  // Thêm const "used"(?)
 
-    // Hàm này để bấm Submit thì nó sẽ đẩy vô json nè
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  // Hàm này để bấm Submit thì nó sẽ đẩy vô json nè
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-
-        const category = {
-            categoryName,
-            created_at,
-            updated_at,
-        };
-
-        setIsPending(true);
-
-        try {
-            const response = await axios.post("http://localhost:8000/categories",
-                JSON.stringify(category), {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
-
-            console.log(response);
-            console.log("New category added");
-            setIsPending(false);
-        } catch (error) {
-            console.log(`ERROR: ${error}`);
-        }
+    const category = {
+      categoryName,
+      created_at,
+      updated_at,
     };
 
-    return (
-        <Box>
-            <Box
-                sx={{
-                    border: 1,
-                    borderColor: "white",
-                    boxShadow: 4,
-                    borderRadius: "25px",
-                    margin: "2rem 3rem",
-                    padding: "2rem",
-                    maxWidth: "100%",
-                }}
-            >
-                <TitleFrame>Create Category</TitleFrame>
-                <Divider
-                    sx={{
-                        marginBottom: "1.5rem",
-                    }}
-                />
+    setIsPending(true);
 
-                <form onSubmit={handleSubmit}>
-                    <Box sx={{ display: "flex", justifyContent: "center" }}>
-                        <TextField
-                            id="outlined-basic"
-                            type="text"
-                            label="New Category"
-                            variant="outlined"
-                            name="tag" //Đổi chỗ này nếu có đổi trên const [tag...]
-                            placeholder={categoryName} //Đổi chỗ này nếu có đổi trên const [tag...]
-                            onChange={(e) => setCategoryName(e.target.value)} //Đổi chỗ này nếu có đổi trên const [tag...]
-                            size="small"
-                            sx={{
-                                width: "50%",
-                                alignSelf: "center",
-                            }}
-                        />
-                        <Box>
-                            {/* NÚT TẠO Ở ĐÂY */}
-                            <Button
-                                type="submit"
-                                variant="contained"
-                                startIcon={<SendIcon />}
-                                sx={{ marginLeft: "1rem", marginRight: "1rem" }}
-                            >
-                                Create
-                            </Button>
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/categories",
+        JSON.stringify(category),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-                            {/* NÀY NÚT RETURN HOME */}
-                            <Button
-                                component={RouterLink}
-                                to="/"
-                                variant="contained"
-                                color="secondary"
-                                sx={{ margin: "1rem 0rem" }}
-                            >
-                                Cancel
-                            </Button>
-                        </Box>
-                    </Box>
-                </form>
+      console.log(response);
+      console.log("New category added");
+      setIsPending(false);
+    } catch (error) {
+      console.log(`ERROR: ${error}`);
+    }
+  };
+
+  return (
+    <Box>
+      <Box
+        sx={{
+          border: 1,
+          borderColor: "white",
+          boxShadow: 4,
+          borderRadius: "25px",
+          margin: "2rem 3rem",
+          padding: "2rem",
+          maxWidth: "100%",
+        }}
+      >
+        <TitleFrame>Create Category</TitleFrame>
+        <Divider
+          sx={{
+            marginBottom: "1.5rem",
+          }}
+        />
+
+        <form onSubmit={handleSubmit}>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <TextField
+              id="outlined-basic"
+              type="text"
+              label="New Category"
+              variant="outlined"
+              name="tag" //Đổi chỗ này nếu có đổi trên const [tag...]
+              placeholder={categoryName} //Đổi chỗ này nếu có đổi trên const [tag...]
+              onChange={(e) => setCategoryName(e.target.value)} //Đổi chỗ này nếu có đổi trên const [tag...]
+              size="small"
+              sx={{
+                width: "50%",
+                alignSelf: "center",
+              }}
+            />
+            <Box>
+              {/* NÚT TẠO Ở ĐÂY */}
+              <Button
+                type="submit"
+                variant="contained"
+                startIcon={<SendIcon />}
+                sx={{ marginLeft: "1rem", marginRight: "1rem" }}
+              >
+                Create
+              </Button>
+
+              {/* NÀY NÚT RETURN HOME */}
+              <Button
+                component={RouterLink}
+                to="/"
+                variant="contained"
+                color="secondary"
+                sx={{ margin: "1rem 0rem" }}
+              >
+                Cancel
+              </Button>
             </Box>
-            <Box sx={{
-                border: 1,
-                borderColor: "white",
-                boxShadow: 4,
-                borderRadius: "25px",
-                margin: "2rem 3rem",
-                padding: "2rem",
-                maxWidth: "100%",
-            }}>
-                <TitleFrame>Category/Tags List</TitleFrame>
-
-                {/* <table className="table table-stripped">
-        <tbody>{display}</tbody>
-      </table> */}
-            </Box>
-        </Box>
-
-    );
+          </Box>
+        </form>
+      </Box>
+      <Box
+        sx={{
+          border: 1,
+          borderColor: "white",
+          boxShadow: 4,
+          borderRadius: "25px",
+          margin: "2rem 3rem",
+          padding: "2rem",
+          maxWidth: "100%",
+        }}
+      >
+        <TitleFrame>Category/Tags List</TitleFrame>
+        {/* <Category /> */}
+      </Box>
+    </Box>
+  );
 };
 
 export default CategoryCreate;
--- frontend/src/pages/Employee/Employees.js
+++ frontend/src/pages/Employee/Employees.js
@@ -1,24 +1,32 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
-// import NavBar from "../../components/Header/NavBar";
 import SearchFunction from "../../components/Search/SearchFunction";
-import EnhancedTable from "./EmployeeTable";
+// import EnhancedTable from "./EmployeeTable";
 import CreateStaffBtn from "../../components/Staff/StaffButtons";
 import EmployeeTable from "./EmployeeTable";
+import { Box, Divider } from "@mui/material";
 
 const Employees = () => {
   // const searchParams = useSearchParams();
 
   return (
-    <div>
-      <SearchFunction page="employees"></SearchFunction>
-      <CreateStaffBtn />
+    <Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "flex-end",
+          m: 3,
+          paddingRight: "3rem",
+        }}
+      >
+        <SearchFunction page="employees"></SearchFunction>
+        <CreateStaffBtn />
+      </Box>
 
       <EmployeeTable></EmployeeTable>
       {/* <EnhancedTable></EnhancedTable> */}
-    </div>
+    </Box>
   );
 };
 
 export default Employees;
-
