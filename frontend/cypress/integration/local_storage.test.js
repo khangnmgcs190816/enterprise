79041ec4c549bff5b8f1c0ee864e3f4bbf0daae9
pngoc.khanhvy <53236281+pnkv12@users.noreply.github.com>
2022-03-30 20:50:00 +0700

    xóa yarn.lock

--- frontend/cypress/integration/local_storage.test.js
+++ frontend/cypress/integration/local_storage.test.js
@@ -1,36 +1,35 @@
 /// <reference types="cypress" />
 const BASE_URL = "http://localhost:3000";
-const token = Cypress.env('authToken');
+const token = Cypress.env("authToken");
 
-context('Local Storage', () => {
+context("Local Storage", () => {
   beforeEach(() => {
     cy.visit(`${BASE_URL}/login`);
-  })
+  });
 
-  it('cy.clearLocalStorage() - clear all data in local storage', () => {
+  it("cy.clearLocalStorage() - clear all data in local storage", () => {
     // log in
     cy.get('input[name="username"]')
       .should("be.visible")
-      .type("trong@gmail.com")
-      .get('input[name="password"]')
-      .should("be.visible")
-      .type("trongvip")
-      .get('button[type="submit"]')
+      .type("trong@gmail.com");
+    cy.get('input[name="password"]').should("be.visible").type("trongvip");
+    cy.get('button[type="submit"]')
       .should("be.visible")
       .click()
       .should(() => {
-        assert.isDefined(localStorage.getItem('authToken'), token)
-        expect(localStorage.getItem('firstName')).to.eq('Trong')
-        expect(localStorage.getItem('email')).to.eq('trong@gmail.com')
+        assert.isDefined(localStorage.getItem("authToken"), token);
+        expect(localStorage.getItem("firstName")).to.eq("Trong");
+        expect(localStorage.getItem("email")).to.eq("trong@gmail.com");
       });
 
     // log out
     cy.get('[data-testid="PersonIcon"]').click();
-    cy.xpath('//p[text()=" Logout"]/parent::li').click()
+    cy.xpath('//p[text()=" Logout"]/parent::li')
+      .click()
       .should(() => {
-        assert.isNull(localStorage.getItem('authToken'));
-        assert.isNull(localStorage.getItem('firstName'))
-        assert.isNull(localStorage.getItem('email'))
+        assert.isNull(localStorage.getItem("authToken"));
+        assert.isNull(localStorage.getItem("firstName"));
+        assert.isNull(localStorage.getItem("email"));
       });
-  })
-})
+  });
+});
